c1a3e4beed4c65b79cc8280339b9bbf0d1705e7e
Adwait <adwaitv10@gmail.com>
2018-04-20 15:01:07 +0530

    converted to method:

---
+++ asgn4_main/second_pass/secondpass.py
@@ -0,0 +1,40 @@
+import sys
+
+def secondpass(lines):
+	lines = lines.split('\n')
+	line_filtered = [t.strip().split(',') for t  in lines]
+
+	label_dict = {}
+	label_line = {}
+	count = 0
+	result = []
+	for i,t in enumerate(line_filtered):
+		if t[0] == 'label':
+			label_dict[t[1]] = count
+			label_line[t[1]] = i - count
+			count+=1
+		else:
+			result.append(t)
+
+	ans = []
+
+	for lineno , line in enumerate(result):
+		for ind,temp in enumerate(line):
+			if(temp in label_line.keys()):
+				line[ind] = label_line[temp] + 1
+
+	result.append(["exit 0"])
+	for i,t in enumerate(result,1):
+		print(t)
+		t.insert(0,str(i))
+		for i,temp in enumerate(t):
+			t[i] = str(temp)
+		t = ' '.join(t)
+		ans.append(t)
+
+	f = open('result.txt','w')
+
+	for t in ans:
+
+		f.write(t+"\n")
+	f.close()
